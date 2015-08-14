16bf302fa6b2940b946b7b95ed543a28f3127c4c
Edwin Daniels <edwin.daniels@ozprinciple.com>
2015-08-14 11:05:33 -0700

    updated demo branch

--- dGrowl/main.js
+++ dGrowl/main.js
@@ -1,10 +1,10 @@
-define([ "dojo/text", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/topic", "dojo/dom-construct", "dGrowl/NotificationNode", "dojo/query", "dojo/_base/lang", "dojo/dom-class"],
-	   function(t, declare, base, templated, topic, domCon, NotificationNode, query, lang, domClass)
+define([ "dojo/text", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/topic", "dojo/dom-construct", "dGrowl/NotificationNode", "dojo/query", "dojo/_base/lang", "dojo/dom-class", "dojo/text!./main.html"],
+	   function(t, declare, base, templated, topic, domCon, NotificationNode, query, lang, domClass, templateString)
 {
 	return declare('dGrowl',
 	[base, templated],
 	{
-		'templateString':dojo.cache('dGrowl', 'main.html'),
+		'templateString':templateString,
 		'channels':[{name:'default', pos:0}], // channel user definitions
 		'orientation':'topRight',
 		'defaultChannel':null,
@@ -46,7 +46,7 @@ define([ "dojo/text", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Templat
 		},
 		'_subscribedChannels':function()
 		{
-			this.addNotification(arguments[0], arguments[1])
+			return this.addNotification(arguments[0], arguments[1])
 		},
 		'addChannel':function(c, o)
 		{
@@ -70,9 +70,11 @@ define([ "dojo/text", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Templat
 				var n = new NotificationNode(o);
 				domCon.place(n.domNode, this.chanDefs[o.channel].node); // push to page
 				n.show();
+				return n;
 			}
 			else
 				console.error(o.channel + ' channel was not found!');
+			return false;
 		},
 		// sorts the channel definitions based on the pos numeric property
 		'sortChannels':function(a,b)
